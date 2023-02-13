00a59840dee103e9d463e3500319261df54df271
JoonaApe <joona.apell@metropolia.fi>
2023-02-13 13:55:40 +0200

    testi

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import mysql.connector
 yhteys = mysql.connector.connect(
     host='localhost',
     port='3306',
-    database='flight_game',
+    database='demogame',
     user='user1',
     password='sala1',
     autocommit=True)
