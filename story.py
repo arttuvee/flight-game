a1baa1a682080fbbca7d299942463183e64f3ada
JoonaApe <joona.apell@metropolia.fi>
2023-02-16 13:24:47 +0200

    lisätty tiedosto josta pelin tausta tarina tuodaan pääohjelmaa

---
+++ story.py
@@ -0,0 +1,13 @@
+import textwrap
+
+story = "Testi testi testi testi testi testi"
+
+
+# Set column width to 80 characters
+wrapper = textwrap.TextWrapper(width=80, break_long_words=False, replace_whitespace=False)
+# Wrap text
+word_list = wrapper.wrap(text=story)
+
+
+def getStory():
+    return word_list
