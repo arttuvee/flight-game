72edaa0f8b782d9b041ba10151424b0dc8f4ff44
arttuvee <arttuvo@metropolia.fi>
2023-02-28 12:44:52 +0200

    hommia = tehty

---
+++ rules.py
@@ -0,0 +1,9 @@
+import textwrap
+
+rules = "sääntöjä jne....." #TODO Joona lisää tähän ne sun muotoilemat säännöt
+
+wrapper = textwrap.TextWrapper(width=80, break_long_words=False, replace_whitespace=False)
+word_list = wrapper.wrap(text=rules)
+
+def getRules():
+    return word_list
